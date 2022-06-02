64462c8fb04fa84b6dfff182c2c57fa06d07a7f3
Eric Ian Noronha Junqueira <ericiannj@gmail.com>
2022-06-02 10:42:42 -0300

    chore: adjust eslint, prettier and editorconfig

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return (
-    <h1>Next TS Boilerplate</h1>
-  )
+    return <h1>Next TS Boilerplate</h1>
 }
 
 export default Home
